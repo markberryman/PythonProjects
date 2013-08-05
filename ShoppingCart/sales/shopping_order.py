933c74ceb9e7831ae82fbde08b4d6568f232c432
Mark Berryman <markwberryman@yahoo.com>
2013-08-05 07:55:50 -0700

    splitting shopping cart/order object into a package

--- ShoppingCart/sales/shopping_order.py
+++ ShoppingCart/sales/shopping_order.py
@@ -1,27 +1,3 @@
-class Cart:
-    def __init__(self):
-        self._contents = dict()
-
-    # controls behavior of "print" method call
-    def __repr__(self):
-        # the __dict__ property contains the internal
-        # contents of an object
-        return "{0} {1}".format(Cart, self.__dict__)
-
-    def process(self, order):
-        if order.add:
-            if not order.item in self._contents:
-                self._contents[order.item] = 1
-            else:
-                self._contents[order.item] += 1
-        elif order.delete:
-            if order.item in self._contents:
-                self._contents[order.item] -= 1
-
-                if self._contents[order.item] == 0:
-                    del self._contents[order.item]
-        
-
 class Order:
     def __init__(self):
         self.quit = False
