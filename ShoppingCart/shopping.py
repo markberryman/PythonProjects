f184b76f3aa98595d4b06397d624091054dd3151
Mark Berryman <markwberryman@yahoo.com>
2013-08-05 07:47:23 -0700

    add method to print cart contents and a bug fix for adding items

--- ShoppingCart/shopping.py
+++ ShoppingCart/shopping.py
@@ -2,12 +2,18 @@ class Cart:
     def __init__(self):
         self._contents = dict()
 
+    # controls behavior of "print" method call
+    def __repr__(self):
+        # the __dict__ property contains the internal
+        # contents of an object
+        return "{0} {1}".format(Cart, self.__dict__)
+
     def process(self, order):
         if order.add:
             if not order.item in self._contents:
                 self._contents[order.item] = 1
-
-            self._contents[order.item] += 1
+            else:
+                self._contents[order.item] += 1
         elif order.delete:
             if order.item in self._contents:
                 self._contents[order.item] -= 1
@@ -78,3 +84,5 @@ while not order.quit:
     cart.process(order)
     order = Order()
     order.get_input()
+
+print(cart)
