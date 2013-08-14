59661c805fe8023e1fb79a2770235a35666e0412
Mark Berryman <markwberryman@yahoo.com>
2013-08-14 08:16:39 -0700

    updating unit test

--- LinkChecker/htmlLinkParserTests.py
+++ LinkChecker/htmlLinkParserTests.py
@@ -11,14 +11,16 @@ class HandleStartTagTests(unittest.TestCase):
         self.assertEqual(0, len(sut.links))
 
     def test_ReturnsLinkFromMarkupWithAnchorTag(self):
-        dummyLink = "http://www.foo.com"
-        dummyMarkup = "<html><a href=\"" + dummyLink + "\"></a></html>";
+        dummyLink1 = "http://www.foo.com"
+        dummyLink2 = "http://www.bar.com"
+        dummyMarkup = "<html>" + "<a href=\"" + dummyLink1 + "\"></a>" + "<a href=\"" + dummyLink2 + "\"></a>" + "</html>";
         sut = htmlLinkParser.HTMLLinkParser()
         
         sut.feed(dummyMarkup)
 
-        self.assertEqual(1, len(sut.links))
-        self.assertTrue(dummyLink in sut.links)
+        self.assertEqual(2, len(sut.links))
+        self.assertTrue(dummyLink1 in sut.links)
+        self.assertTrue(dummyLink2 in sut.links)
 
 if __name__ == '__main__':
     unittest.main()
