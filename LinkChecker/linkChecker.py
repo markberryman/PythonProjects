a1dfd411091db68ed615c7a6c8c393a5abc6f24c
Mark Berryman <markwberryman@yahoo.com>
2013-08-15 21:03:05 -0700

    refactoring a single link processing operation to new method

--- LinkChecker/linkChecker.py
+++ LinkChecker/linkChecker.py
@@ -30,28 +30,32 @@ class LinkChecker:
         else:
             print("No broken links.")
 
-    def do_stuff(self):
-        return None
+    # todo - add unit tests
+    def process_link(self, link):
+        """Returns the new links detected from processing a link."""
+        # todo - should we block leaving the root domain?
+        isLinkBroken, markup = self.linkRequester.get_link(link)
+                
+        newLinks = None
 
-    # todo - this function is doing too much; break it up and/or move static code to utility class    
+        if (isLinkBroken):
+            self.brokenLinks.add(link)                
+        else:
+            # todo - convert relative links to absolute links
+            self.htmlLinkParser.feed(markup)                    
+            newLinks = self.htmlLinkParser.links
+            print("Parse links returning {0} links".format(len(newLinks)))
+
+        return newLinks
+
+    # todo - add test; won't be unit tests though    
     def check_links_helper(self, linksToProcess, curDepth):
         """Checks the provided set of links but not beyond the specified depth."""
         if (curDepth <= self.maxDepth):
             for link in linksToProcess:             
-                # todo - should we block leaving the root domain?
-                isLinkBroken, markup = self.linkRequester.get_link(link)
-                
-                if (isLinkBroken):
-                    self.brokenLinks.add(link)                
-                else:
-                    # todo - convert relative links to absolute links
-                    self.htmlLinkParser.feed(markup)                    
+                newLinks = self.process_link(link)
 
-                    newLinks = self.htmlLinkParser.links
-
-                    print("Parse links returning {0} links".format(len(newLinks)))
-
-                    self.check_links_helper(newLinks, curDepth + 1)
+                self.check_links_helper(newLinks, curDepth + 1)
 
             self.numLinksProcessed += len(linksToProcess)
 
