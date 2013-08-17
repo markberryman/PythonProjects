b177930164a6b585f4ec8f8f5b423e04f475f656
Mark Berryman <markwberryman@yahoo.com>
2013-08-16 20:57:12 -0700

    * making get_links_from_markup static * +unit tests

--- LinkChecker/linkCheckerUtilities.py
+++ LinkChecker/linkCheckerUtilities.py
@@ -1,7 +1,8 @@
 class linkCheckerUtilities(object):
     """Various utility methods"""
 
-    def get_links_from_markup(self, markup, htmlLinkParser):
+    @staticmethod
+    def get_links_from_markup(markup, htmlLinkParser):
         """Returns the new links detected from processing a link."""
         if (markup is None):
             return None
