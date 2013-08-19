ab7a5dbce51cdeaf5a6f900930414fc1f8e5ae67
Mark Berryman <markwberryman@yahoo.com>
2013-08-19 13:41:03 -0700

    inlining __process_link method

--- LinkChecker/linkChecker.py
+++ LinkChecker/linkChecker.py
@@ -31,25 +31,20 @@ class LinkChecker:
         else:
             print("No broken links.")
 
-    # todo - add test; won't be unit test though
-    def __process_link(self, link):
-        """Requests a link, checks if its broken and sends the related markup for processing."""
-        # todo - should we block leaving the root domain?
-        markup = self.linkRequester.get_link(link)
-
-        if (markup is None):
-            self.brokenLinks.add(link)
-        
-        return linkCheckerUtilities.linkCheckerUtilities.get_links_from_markup(markup, self.htmlLinkParser)
-
     # todo - add test; won't be unit tests though    
     def __check_links_helper(self, linksToProcess, curDepth):
         """Checks the provided set of links but not beyond the specified depth."""
         if (curDepth <= self.maxDepth):
-            for link in linksToProcess:             
-                newLinks = self.__process_link(link)
+            for link in linksToProcess:
+                # todo - should we block leaving the root domain?
+                markup = self.linkRequester.get_link(link)
 
-                self.check_links_helper(newLinks, curDepth + 1)
+                if (markup is None):
+                    self.brokenLinks.add(link)
+        
+                newLinks = linkCheckerUtilities.linkCheckerUtilities.get_links_from_markup(markup, self.htmlLinkParser)
+
+                self.__check_links_helper(newLinks, curDepth + 1)
 
             self.numLinksProcessed += len(linksToProcess)
 
@@ -61,6 +56,6 @@ class LinkChecker:
         linksToProcess = set()
         linksToProcess.add(self.startLink)
 
-        self.check_links_helper(linksToProcess, 1)
+        self.__check_links_helper(linksToProcess, 1)
         
         return None
