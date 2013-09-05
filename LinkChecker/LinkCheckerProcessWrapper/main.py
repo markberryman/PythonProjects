ee5702280717ff2a4a2137cdf18c719c165aa522
Mark Berryman <markwberryman@yahoo.com>
2013-09-05 12:44:12 -0700

    using separate file to store azure creds; git ignoring that file

--- LinkChecker/LinkCheckerProcessWrapper/main.py
+++ LinkChecker/LinkCheckerProcessWrapper/main.py
@@ -1,6 +1,9 @@
-from azure.storage import *
+from azure.storage import QueueService
+import azureStorageCreds
 
 # connect to queue
+queue_service = QueueService(azureStorageCreds.STORAGE_ACCOUNT, azureStorageCreds.STORAGE_ACCOUNT_KEY)
+queue_service.create_queue('testqueue')
 
 # in a loop
     # get work
