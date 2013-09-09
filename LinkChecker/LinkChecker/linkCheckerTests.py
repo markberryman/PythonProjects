83853a5bc86cc8af8b404e71b42c55803305811e
Mark Berryman <markwberryman@yahoo.com>
2013-09-09 07:29:55 -0700

    -dead code

--- LinkChecker/LinkChecker/linkCheckerTests.py
+++ LinkChecker/LinkChecker/linkCheckerTests.py
@@ -1,11 +1,4 @@
-﻿#import html.parser
-#from html.parser import HTMLParser
-#import link
-#import linkChecker
-#import resourceGetter
-
-import contentRequester
-import htmlLinkParser
+﻿import contentRequester
 import htmlLinkParserFactory
 import link
 import linkChecker
@@ -13,65 +6,8 @@ import linkFilter
 import resourceGetter
 import unittest
 
-#class MockHtmlLinkParser(HTMLParser):
-#    def __init__(self):
-#        super().__init__(self)
-#        self.links = set()
-
-#    def feed(self, data):
-#        raise html.parser.HTMLParseError("error")
-
-#    def handle_starttag(self, tag, attrs):
-#        return None
-
-#class MockHtmlLinkParserFactory(object):
-#    def create_html_link_parser(self):
-#        return MockHtmlLinkParser()
-    
-#class MockLinkRequester(object):
-#    def __init__(self, dummyMarkupToReturn = "dummy markup", dummyStatusCodeToReturn = 200):
-#        self.dummyMarkupToReturn = dummyMarkupToReturn
-#        self.dummyStatusCodeToReturn = dummyStatusCodeToReturn
-
-#    def get_resource(self, link):
-#        return self.dummyStatusCodeToReturn, self.dummyMarkupToReturn
-
-#class MockLinkFilter(object):
-#    def filter_links(self, links):
-#        return links
-
 # these are more functional tests rather than unit tests
 class CheckLinksTests(unittest.TestCase):
-    #def test_CheckLinksFunctionalTest(self):
-    #    mockHtmlLinkParserFactory = MockHtmlLinkParserFactory()
-    #    mockLinkRequester = MockLinkRequester()
-    #    dummyLink = link.Link("some link", link.LinkType.ANCHOR)
-    #    sut = linkChecker.LinkChecker(mockHtmlLinkParserFactory, mockLinkRequester, None)
-
-    #    sut.check_links(set([dummyLink]), 1)
-
-    #    self.assertEqual(1, sut.numLinksProcessed)
-
-    #def test_CheckLinksAddsBrokenLink(self):
-    #    mockHtmlLinkParserFactory = MockHtmlLinkParserFactory()
-    #    mockLinkRequester = MockLinkRequester(None, 404)
-    #    dummyLink = link.Link("broken link", link.LinkType.ANCHOR)
-    #    sut = linkChecker.LinkChecker(mockHtmlLinkParserFactory, mockLinkRequester, None)
-
-    #    sut.check_links(set([dummyLink]), 1)
-
-    #    self.assertEqual(1, len(sut.brokenLinks))
-
-    #def test_CheckLinksTracksPagesWithInvalidMarkup(self):
-    #    mockHtmlLinkParserFactory = MockHtmlLinkParserFactory()
-    #    mockLinkRequester = MockLinkRequester()
-    #    dummyLink = link.Link("link with invalid markup", link.LinkType.ANCHOR)
-    #    sut = linkChecker.LinkChecker(mockHtmlLinkParserFactory, mockLinkRequester, None)
-
-    #    sut.check_links(set([dummyLink]), 1)
-
-    #    self.assertEqual(1, len(sut.invalidMarkupLinks))
-
     def test_FunctionalE2ETest(self):
         startLink = link.Link("http://localhost:35944/index.html")
         depth = 2
