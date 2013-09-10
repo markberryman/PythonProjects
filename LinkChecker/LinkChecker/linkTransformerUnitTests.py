2edff2bc8e70e0b15be8edc83e0cfe5579490dee
Mark Berryman <markwberryman@yahoo.com>
2013-09-10 07:30:55 -0700

    renaming file

--- LinkChecker/LinkChecker/linkTransformerUnitTests.py
+++ LinkChecker/LinkChecker/linkTransformerUnitTests.py
@@ -5,18 +5,18 @@ class RelativeLinkTransformerUnitTests(unittest.TestCase):
     def test_TransformsRelativeLink(self):
         dummyCurrentLink = "http://www.foo.com"
         dummyNewLink = "relativelink.html"
-        sut = linkTransformer.RelativeLinkTransformer()
+        sut = linkTransformer.RelativeLinkTransformer(dummyCurrentLink)
 
-        result = sut.transform(dummyCurrentLink, dummyNewLink)
+        result = sut.transform(dummyNewLink)
 
         self.assertEqual(result, "http://www.foo.com/relativelink.html")
         
     def test_DoesNotTransformsAbsoluteLink(self):
         dummyCurrentLink = "http://www.foo.com"
         dummyNewLink = "http://www.bar.com"
-        sut = linkTransformer.RelativeLinkTransformer()
+        sut = linkTransformer.RelativeLinkTransformer(dummyCurrentLink)
 
-        result = sut.transform(dummyCurrentLink, dummyNewLink)
+        result = sut.transform(dummyNewLink)
 
         self.assertEqual(result, dummyNewLink)
 
