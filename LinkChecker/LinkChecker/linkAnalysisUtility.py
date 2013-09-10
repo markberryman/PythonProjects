042f0f51c5a00807be1d1fd48389491f0f5d0c3c
Mark Berryman <markwberryman@yahoo.com>
2013-09-10 14:07:55 -0700

    deleting unused classes

--- LinkChecker/LinkChecker/linkAnalysisUtility.py
+++
@@ -1,6 +0,0 @@
-class LinkAnalysisUtility:
-    @staticmethod
-    def is_link_relative(link):
-        isRelative = not link.lower().startswith("http")
-
-        return isRelative
--- LinkChecker/LinkChecker/linkConverterUtility.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import urlunparse
-
-
-class LinkConverterUtility:
-    @staticmethod
-    def convert_relative_link_to_absolute_link(host, link):
-        return host + link
