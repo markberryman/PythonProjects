02e39b5afe5e6ed0779b5e3bdb8f1160f07b03b0
Mark Berryman <markwberryman@yahoo.com>
2013-09-11 10:33:55 -0700

    adding user-agent header to request

--- LinkChecker/LinkChecker/contentRequester.py
+++ LinkChecker/LinkChecker/contentRequester.py
@@ -7,7 +7,11 @@ class ContentRequester(object):
     def request_url(self, url):
         urlParts = urlparse(url)
 
+        # need to include some user agent value otherwise
+        # sites are rejecting the request
+        headers = {"User-Agent": "linkChecker"}
+
         conn = http.client.HTTPConnection(urlParts.netloc)
-        conn.request("GET", urlParts.path)
+        conn.request("GET", urlParts.path, body=None, headers=headers)
 
         return conn.getresponse()
