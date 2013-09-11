c604a21c0f902cbfa743ad7fbbd6af2ec33ca618
Mark Berryman <markwberryman@yahoo.com>
2013-09-11 11:11:35 -0700

    [bf] - handling anchor tags that point to binary content

--- LinkChecker/LinkChecker/resourceGetter.py
+++ LinkChecker/LinkChecker/resourceGetter.py
@@ -12,12 +12,20 @@ class ResourceGetter:
         print("Getting url \"{0}\"".format(linkToProcess.value))
 
         res = self.contRequester.request_url(linkToProcess.value)
+
         statusCode = res.status
 
         print("Response status = {0}".format(statusCode))
 
         # only want to fetch the content of anchor links
         if (linkToProcess.type == link.LinkType.ANCHOR):
-            responseData = res.read().decode()
+            try:
+                responseData = res.read().decode()
+            except UnicodeDecodeError:
+                # going to hit this when an anchor link refers to a binary
+                # resource (e.g. pdf file); instead of trying to filter
+                # out all of these extensions, just swallow the exception
+                # and move on for now
+                pass
 
         return statusCode, responseData
