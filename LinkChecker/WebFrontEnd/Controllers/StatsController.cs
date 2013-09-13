f324fe6ea87cf91f5c5a781410d84bc890bb1d56
Mark Berryman <markwberryman@yahoo.com>
2013-09-13 12:36:43 -0700

    adding test web api endpoint

---
+++ LinkChecker/WebFrontEnd/Controllers/StatsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebFrontEnd.Controllers {
+    public class StatsController : ApiController {
+        public struct SystemStats {
+            public DateTime StatsDate { get; set; }
+            public long NumJobsProcessed { get; set; }
+        }
+
+        // GET api/stats
+        public SystemStats Get() {
+            var ss = new SystemStats {
+                StatsDate = DateTime.Now,
+                NumJobsProcessed = DateTime.Now.Ticks
+            };
+
+            return ss;
+        }
+    }
+}
