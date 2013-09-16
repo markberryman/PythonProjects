dd1b7063a5159fbb37d885e2f738b6dae60b40ef
Mark Berryman <markwberryman@yahoo.com>
2013-09-16 15:56:32 -0700

    +unit tests

--- DocumentDistance/test/generateWordListTests.py
+++ DocumentDistance/test/generateWordListTests.py
@@ -3,11 +3,18 @@ import unittest
 
 class GenerateWordListTests(unittest.TestCase):
     def test_ReturnsCorrectWordCountForVerneTextFile(self):
-        filename = "..\\t1.vern.txt"
+        filename = "../t1.verne.txt"
 
         result = docdist1.generateWordList(filename)
 
-        self.assertEqual(2150, len(result))
+        self.assertEqual(8943, len(result))
+
+    def test_ReturnsCorrectWordCountForBobseyTextFile(self):
+        filename = "../t2.bobsey.txt"
+
+        result = docdist1.generateWordList(filename)
+
+        self.assertEqual(49785, len(result))
 
 if __name__ == '__main__':
     unittest.main()
