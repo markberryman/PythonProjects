68108b3700fe17057ae665e25d08309bd7e23404
Mark Berryman <markwberryman@yahoo.com>
2013-09-16 15:49:39 -0700

    getting setup to add unit tests

---
+++ DocumentDistance/test/generateWordListTests.py
@@ -0,0 +1,13 @@
+﻿import docdist1
+import unittest
+
+class GenerateWordListTests(unittest.TestCase):
+    def test_ReturnsCorrectWordCountForVerneTextFile(self):
+        filename = "..\\t1.vern.txt"
+
+        result = docdist1.generateWordList(filename)
+
+        self.assertEqual(2150, len(result))
+
+if __name__ == '__main__':
+    unittest.main()
