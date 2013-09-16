11e229473ae7af0e62357acd9b6a2d68ff2cdf5e
Mark Berryman <markwberryman@yahoo.com>
2013-09-16 15:54:35 -0700

    adding a main module

--- DocumentDistance/docdist1.py
+++ DocumentDistance/docdist1.py
@@ -212,12 +212,3 @@ def calculateAngleMetric(wordList1, wordList2):
     result = math.acos(round(tempresult, 15))
 
     return result
-
-# Expected test results
-#File t1.verne.txt : 1057 lines, 8943 words, 2150 distinct words
-#File t2.bobsey.txt : 6667 lines, 49785 words, 3354 distinct words
-#The distance between the documents is: 0.582949 (radians)
-
-documentDist("t1.verne.txt", "t2.bobsey.txt")
-
-input('Press Enter to exit')
---
+++ DocumentDistance/main.py
@@ -0,0 +1,10 @@
+import docdist1
+
+# Expected test results
+#File t1.verne.txt : 1057 lines, 8943 words, 2150 distinct words
+#File t2.bobsey.txt : 6667 lines, 49785 words, 3354 distinct words
+#The distance between the documents is: 0.582949 (radians)
+
+docdist1.documentDist("t1.verne.txt", "t2.bobsey.txt")
+
+input('Press Enter to exit')
