0448c39bd066c7007ffa7d48bde50b26fe55aa86
Mark Berryman <markwberryman@yahoo.com>
2013-09-16 20:37:29 -0700

    +unit test

--- DocumentDistance/test/wordCounterTests.py
+++ DocumentDistance/test/wordCounterTests.py
@@ -12,5 +12,15 @@ class SimpleWordCounterTests(unittest.TestCase):
 
         self.assertEqual(8943, len(result))
 
+    def test_CountWordsReturnsCorrectCountOfWordsInBobsey(self):
+        filename = "../t2.bobsey.txt"
+        sut = wordCounter.SimpleWordCounter()
+        input = sut.read_file(filename)
+
+        result = sut.count_words(input)
+
+        self.assertEqual(49785, len(result))
+
+
 if __name__ == '__main__':
     unittest.main()
