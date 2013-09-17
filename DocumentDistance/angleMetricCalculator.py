9bde3d2ef105b677772ae10c704338f532598e4b
Mark Berryman <markwberryman@yahoo.com>
2013-09-17 09:06:48 -0700

    adding vector inner product calculation and tests

--- DocumentDistance/angleMetricCalculator.py
+++ DocumentDistance/angleMetricCalculator.py
@@ -15,3 +15,22 @@ class AngleMetricCalculator(object):
         norm = math.sqrt(dotProduct)
 
         return norm
+
+    def calc_inner_product(self, listOfWordFrequencies1, listOfWordFrequencies2):
+        """Calculate the inner (or dot) product of the two vectors 
+        representing word frequencies. The calculation is the summation 
+        of multiplying corresponding elements from each vector."""
+        length1 = len(listOfWordFrequencies1)
+        length2 = len(listOfWordFrequencies2)
+        innerProduct = 0
+
+        # any values in one list that don't have a corresponding match
+        # in the other equal zero so we ignore them
+        if (length1 < length2):
+            for idx, val in enumerate(listOfWordFrequencies1):
+                innerProduct += listOfWordFrequencies1[idx] * listOfWordFrequencies2[idx]
+        else:
+            for idx, val in enumerate(listOfWordFrequencies2):
+                innerProduct += listOfWordFrequencies1[idx] * listOfWordFrequencies2[idx]
+
+        return innerProduct
--- DocumentDistance/test/angleMetricCalculatorTests.py
+++ DocumentDistance/test/angleMetricCalculatorTests.py
@@ -21,5 +21,24 @@ class CalcNormTests(unittest.TestCase):
         self.assertEqual(math.sqrt(13), result)
 
 
+class CalcInnerProductTests(unittest.TestCase):
+    def test_InnerProductOfTwoWordFreqListsWithSameSize(self):
+        wordFreqs1 = [1, 2]
+        wordFreqs2 = [2, 3]
+        sut = angleMetricCalculator.AngleMetricCalculator()
+
+        result = sut.calc_inner_product(wordFreqs1, wordFreqs2)
+
+        self.assertEqual(8, result)
+
+    def test_InnerProductOfTwoWordFreqListsWithDiffSize(self):
+        wordFreqs1 = [1]
+        wordFreqs2 = [2, 3]
+        sut = angleMetricCalculator.AngleMetricCalculator()
+
+        result = sut.calc_inner_product(wordFreqs1, wordFreqs2)
+
+        self.assertEqual(2, result)
+
 if __name__ == '__main__':
     unittest.main()
