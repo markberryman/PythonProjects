6385a2f105e51b64d531a74bef7fd5e1f7d64316
Mark Berryman <markwberryman@yahoo.com>
2013-09-17 16:01:18 -0700

    heavy code cleanup; dropping the vector norm calc; [bf] - correctly implementing angle calc; adding and fixing unit tests

--- DocumentDistance/angleMetricCalculator.py
+++ DocumentDistance/angleMetricCalculator.py
@@ -4,37 +4,32 @@ import math
 class AngleMetricCalculator(object):
     """Calculates the angle metric b/w two word lists."""
 
-    def calc_norm(self, listOfWordFrequencies):
-        """Calculate the norm of a vector composed of word frequencies.
-        Norm is calculated as the square root of D dot product D."""
-        dotProduct = 0
-
-        for freq in listOfWordFrequencies:
-            dotProduct += freq * freq
-
-        norm = math.sqrt(dotProduct)
-
-        return norm
-    
     def calc_inner_product(self, wordFreqs1, wordFreqs2):
         """Calculate the inner (or dot) product of the two vectors 
         representing word frequencies. The calculation is the summation 
-        of multiplying corresponding elements from each vector."""
-        shorterList = wordFreqs1 if (len(wordFreqs1) < len(wordFreqs2)) else wordFreqs2
+        of multiplying corresponding elements from each vector.
+
+        Helps if the lists of word frequencies are ordered. Otherwise,
+        we're headed to an O(n^2) algorithm here.
+        """
         innerProduct = 0
 
-        # any values in one list that don't have a corresponding match
-        # in the other equal zero so we ignore them
-        for idx, val in enumerate(shorterList):
-            innerProduct += wordFreqs1[idx] * wordFreqs2[idx]
+        for word1, freq1 in wordFreqs1:
+            for word2, freq2 in wordFreqs2:
+                if (word2 == word1):
+                    innerProduct += freq1 * freq2
+                    break
 
         return innerProduct
 
     def calc_vector_angle(self, wordFreqs1, wordFreqs2):
         """Calculate the angle b/w two vectors. The angle is the arccos
-        of their inner product divided by the product of their norms."""
-        innerProduct = self.calc_inner_product(wordFreqs1, wordFreqs2)
-        wordFreqs1Norm = self.calc_norm(wordFreqs1)
-        wordFreqs2Norm = self.calc_norm(wordFreqs2)
-
-        return math.acos(innerProduct / (wordFreqs1Norm * wordFreqs2Norm))
+        of their inner product divided by the product of their norms
+        which can be restated as the square root of their inner
+        products."""
+        numerator = self.calc_inner_product(wordFreqs1, wordFreqs2)
+        denominator = math.sqrt(
+            self.calc_inner_product(wordFreqs1, wordFreqs1) * 
+            self.calc_inner_product(wordFreqs2, wordFreqs2))
+         
+        return math.acos(numerator / denominator)
--- DocumentDistance/test/angleMetricCalculatorTests.py
+++ DocumentDistance/test/angleMetricCalculatorTests.py
@@ -1,57 +1,37 @@
 import angleMetricCalculator
-import math
 import unittest
 
 
-class CalcNormTests(unittest.TestCase):
-    def test_NormOfOneWordFrequency(self):
-        wordFreqs = [2]
-        sut = angleMetricCalculator.AngleMetricCalculator()
-
-        result = sut.calc_norm(wordFreqs)
-        
-        self.assertEqual(2, result)
-
-    def test_NormOfTwoWordFrequencies(self):
-        wordFreqs = [2, 3]
-        sut = angleMetricCalculator.AngleMetricCalculator()
-
-        result = sut.calc_norm(wordFreqs)
-        
-        self.assertEqual(math.sqrt(13), result)
-
-
 class CalcInnerProductTests(unittest.TestCase):
-    def test_InnerProductOfTwoWordFreqListsWithSameSize(self):
-        wordFreqs1 = [1, 2]
-        wordFreqs2 = [2, 3]
+    def test_InnerProductOfTwoWordFreqListsSameSize(self):
+        wordFreqs1 = [("a", 1), ("b", 2)]
+        wordFreqs2 = [("a", 2), ("b", 3)]
         sut = angleMetricCalculator.AngleMetricCalculator()
 
         result = sut.calc_inner_product(wordFreqs1, wordFreqs2)
 
         self.assertEqual(8, result)
 
-    def test_InnerProductOfTwoWordFreqListsWithDiffSize(self):
-        wordFreqs1 = [1]
-        wordFreqs2 = [2, 3]
+    def test_InnerProductOfTwoWordFreqListsDiffSize(self):
+        wordFreqs1 = [("a", 1), ("b", 2)]
+        wordFreqs2 = [("b", 3)]
         sut = angleMetricCalculator.AngleMetricCalculator()
 
         result = sut.calc_inner_product(wordFreqs1, wordFreqs2)
 
-        self.assertEqual(2, result)
+        self.assertEqual(6, result)
 
 class CalcVectorAngleTests(unittest.TestCase):
-    def test_BasicVectorAngleCalc(self):
+    def test_BasicVectorAngleCalcWorks(self):
         # "To be or not to be" and "Doubt truth to be a liar"
-        # common words in each: "to be"
-        wordFreq1 = [2, 2]
-        wordFreq2 = [1, 1]
+        wordFreq1 = [("be", 2), ("to", 2), ("not", 1), ("or", 1)]
+        wordFreq2 = [("a", 1), ("be", 1), ("doubt", 1), ("liar", 1), ("to", 1), ("truth", 1)]
         sut = angleMetricCalculator.AngleMetricCalculator()
 
         result = sut.calc_vector_angle(wordFreq1, wordFreq2)
 
-        self.assertEqual(1.028, result)
-
+        self.assertEqual(1.028, round(result, 3))
+        
 
 if __name__ == '__main__':
     unittest.main()
