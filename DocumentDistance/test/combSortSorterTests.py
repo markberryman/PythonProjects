20fc583026ae7255067699b56e9856d428244e2d
Mark Berryman <markwberryman@yahoo.com>
2013-09-18 09:41:20 -0700

    +unit tests

--- DocumentDistance/test/combSortSorterTests.py
+++ DocumentDistance/test/combSortSorterTests.py
@@ -22,6 +22,19 @@ class CombSortSorterTests(unittest.TestCase):
 
         self.assertEqual(wordTuple1, wordList[0])
 
+    def test_SortsThreeElementWordList(self):
+        wordTuple1 = ("a", 1)
+        wordTuple2 = ("b", 1)
+        wordTuple3 = ("c", 1)
+        wordList = [wordTuple3, wordTuple2, wordTuple1]
+        sut = combSortSorter.CombSortSorter()
+
+        sut.sort_words(wordList)
+
+        self.assertEqual(wordTuple1, wordList[0])
+        self.assertEqual(wordTuple2, wordList[1])
+        self.assertEqual(wordTuple3, wordList[2])
+
     def test_SortsFiveElementWordList(self):
         wordTuple1 = ("a", 1)
         wordTuple2 = ("b", 1)
@@ -34,7 +47,10 @@ class CombSortSorterTests(unittest.TestCase):
         sut.sort_words(wordList)
 
         self.assertEqual(wordTuple1, wordList[0])
-
+        self.assertEqual(wordTuple2, wordList[1])
+        self.assertEqual(wordTuple3, wordList[2])
+        self.assertEqual(wordTuple4, wordList[3])
+        self.assertEqual(wordTuple5, wordList[4])
 
 if __name__ == '__main__':
     unittest.main()
