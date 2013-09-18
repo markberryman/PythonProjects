27394f2fb00f76340c81c3e17685e0f64a0bfe06
Mark Berryman <markwberryman@yahoo.com>
2013-09-18 09:15:57 -0700

    removing unused import; rounding final dist value printed out

--- DocumentDistance/docdist1.py
+++ DocumentDistance/docdist1.py
@@ -1,5 +1,4 @@
 import angleMetricCalculator
-import math
 import wordCounter
 import wordFrequencyCalculator
 
@@ -11,16 +10,16 @@ def documentDist(fileName1, fileName2, sortingAlgo):
     
     inputLines = theWordCounter.read_file(fileName1)
     wordListForFile1 = theWordCounter.count_words(inputLines)
-    wordFrequencyListFile1 = freqCounter.calculate_word_frequency(wordListForFile1)
-    sortingAlgo.sort_words(list(wordFrequencyListFile1.items()))
+    wordFrequencyListFile1 = list(freqCounter.calculate_word_frequency(wordListForFile1).items())
+    sortingAlgo.sort_words(wordFrequencyListFile1)
 
     print("File {0} : {1} words, {2} distinct words".format(fileName1, len(wordListForFile1),
                                                             len(wordFrequencyListFile1)))
 
     inputLines = theWordCounter.read_file(fileName2)
     wordListForFile2 = theWordCounter.count_words(inputLines)
-    wordFrequencyListFile2 = freqCounter.calculate_word_frequency(wordListForFile2)
-    sortingAlgo.sort_words(list(wordFrequencyListFile2.items()))
+    wordFrequencyListFile2 = list(freqCounter.calculate_word_frequency(wordListForFile2).items())
+    sortingAlgo.sort_words(wordFrequencyListFile2)
 
     print("File {0} : {1} words, {2} distinct words".format(fileName2, len(wordListForFile2),
                                                             len(wordFrequencyListFile2)))
@@ -28,7 +27,7 @@ def documentDist(fileName1, fileName2, sortingAlgo):
     angleMetricCalc = angleMetricCalculator.AngleMetricCalculator()
     angleMetric = angleMetricCalc.calc_vector_angle(wordFrequencyListFile1, wordFrequencyListFile2)
     
-    print("The distance between the documents is: {0} radians".format(angleMetric))
+    print("The distance between the documents is: {0} radians".format(round(angleMetric, 6)))
 
 
 ######################
