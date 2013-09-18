a5f4e6067bc2c634723e67717f67dfbe82e08163
Mark Berryman <markwberryman@yahoo.com>
2013-09-18 10:20:52 -0700

    swapping out bubble sort for comb sort

--- DocumentDistance/main.py
+++ DocumentDistance/main.py
@@ -1,6 +1,8 @@
 import bubbleSortSorter
+import combSortSorter
 import docdist1
 
-docdist1.documentDist("t1.verne.txt", "t2.bobsey.txt", bubbleSortSorter.BubbleSortSorter())
+#docdist1.documentDist("t1.verne.txt", "t2.bobsey.txt", bubbleSortSorter.BubbleSortSorter())
+docdist1.documentDist("t1.verne.txt", "t2.bobsey.txt", combSortSorter.CombSortSorter())
 
 input('Press Enter to exit')
