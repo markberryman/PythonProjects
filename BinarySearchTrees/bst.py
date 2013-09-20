f4b79c85c9f5ec31ec960ee8801cb086390f10c1
Mark Berryman <markwberryman@yahoo.com>
2013-09-20 07:29:03 -0700

    renaming classes

---
+++ BinarySearchTrees/bst.py
@@ -0,0 +1,11 @@
+class BST(object):
+    """
+    Binary search tree.
+    """
+    
+    #def select(self, index):
+    #    """
+    #    Takes a 1-based index, and returns the element at that index,
+    #    or None if the index is out-of-bounds.
+    #    """
+    #    return None
---
+++ BinarySearchTrees/bstTests.py
@@ -0,0 +1,33 @@
+import bst
+import unittest
+
+#class TestBST(unittest.TestCase):
+#    def setUp(self):
+#        pass
+    
+#    def test(self):
+#        t = bstselect.BST()
+#        t.insert(0)
+#        t.insert(10)
+#        t.insert(5)
+
+#        ans = t.select(1)
+#        self.assertEqual(ans.key, 0)
+#        ans = t.select(2)
+#        self.assertEqual(ans.key, 5)
+#        ans = t.select(3)
+#        self.assertEqual(ans.key, 10)
+
+#        ans = t.select(0)
+#        self.assertEqual(ans, None)
+#        ans = t.select(4)
+#        self.assertEqual(ans, None)
+
+#        t.insert(6)
+#        t.insert(8)
+#        t.insert(7)
+#        ans = t.select(4)
+#        self.assertEqual(ans.key, 7)
+
+if __name__ == '__main__':
+    unittest.main()
