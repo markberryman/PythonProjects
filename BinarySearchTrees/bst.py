5f96ba7e5919c2566770be0490546e931ea4059a
Mark Berryman <markwberryman@yahoo.com>
2013-09-20 07:30:21 -0700

    adding Node class

--- BinarySearchTrees/bst.py
+++ BinarySearchTrees/bst.py
@@ -1,3 +1,9 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.lchild = None
+        self.rchild = None
+
 class BST(object):
     """
     Binary search tree.
