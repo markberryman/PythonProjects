84013af0e829754830233b973ee73a06f2984391
Mark Berryman <markwberryman@yahoo.com>
2013-09-24 07:50:50 -0700

    renaming file

--- BinaryPlusOne/BinaryPlusOne.py
+++ BinaryPlusOne/BinaryPlusOne.py
@@ -2,26 +2,34 @@ class BinaryPlusOne(object):
     @staticmethod
     def add_one(num):
         """Pass in an array representing an array of bits."""
-        result = None
         # start from rightmost bit
         bitIndex = len(num) - 1
         
-        # look at rightmost bit
-        rBit = num[bitIndex]
-        # if it's a zero, replace w/ a one
-        if (rBit == 0):
-            num[bitIndex] = 1
-        else:
-            # if it's a one, replace w/ a zero
-            num[bitIndex] = 0
-            # now we're in the "carry" phase
-            while (True):
-                # look at next digit (going left)
-                bitIndex -= 1
-                # if it's a zero, replace w/ a one
-                if (num[bitIndex] == 0):
-                    num[bitIndex] = 1
-                    break
-                else:
-                    # if it's a one, replace w/ a zero; continue
-                    num[bitIndex] = 0
+        while (True):
+            if (num[bitIndex] == 0):
+                num[bitIndex] = 1
+                break
+            else:
+                num[bitIndex] = 0
+
+            bitIndex -= 1
+
+        ## look at rightmost bit
+        #rBit = num[bitIndex]
+        ## if it's a zero, replace w/ a one
+        #if (rBit == 0):
+        #    num[bitIndex] = 1
+        #else:
+        #    # if it's a one, replace w/ a zero
+        #    num[bitIndex] = 0
+        #    # now we're in the "carry" phase
+        #    while (True):
+        #        # look at next digit (going left)
+        #        bitIndex -= 1
+        #        # if it's a zero, replace w/ a one
+        #        if (num[bitIndex] == 0):
+        #            num[bitIndex] = 1
+        #            break
+        #        else:
+        #            # if it's a one, replace w/ a zero; continue
+        #            num[bitIndex] = 0
