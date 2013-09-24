6a2eb1b43e65541ffcea56cf5df0b9eda62349ae
Mark Berryman <markwberryman@yahoo.com>
2013-09-24 07:43:22 -0700

    +unit tests

--- BinaryPlusOne/binaryPlusOneTests.py
+++ BinaryPlusOne/binaryPlusOneTests.py
@@ -11,6 +11,46 @@ class AddOneTests(unittest.TestCase):
 
         self.assertEqual(expected, num)
 
+    def test_AddOneToOne(self):
+        num = [0, 1]
+        expected = [1, 0]
+        
+        binaryPlusOne.BinaryPlusOne.add_one(num)
+
+        self.assertEqual(expected, num)
+
+    def test_AddOneToTwo(self):
+        num = [1, 0]
+        expected = [1, 1]
+        
+        binaryPlusOne.BinaryPlusOne.add_one(num)
+
+        self.assertEqual(expected, num)
+
+    def test_AddOneToThree(self):
+        num = [0, 1, 1]
+        expected = [1, 0, 0]
+        
+        binaryPlusOne.BinaryPlusOne.add_one(num)
+
+        self.assertEqual(expected, num)
+
+    def test_AddOneToFour(self):
+        num = [1, 0, 0]
+        expected = [1, 0, 1]
+        
+        binaryPlusOne.BinaryPlusOne.add_one(num)
+
+        self.assertEqual(expected, num)
+
+    def test_AddOneToFive(self):
+        num = [1, 0, 1]
+        expected = [1, 1, 0]
+        
+        binaryPlusOne.BinaryPlusOne.add_one(num)
+
+        self.assertEqual(expected, num)
+
 
 if __name__ == '__main__':
     unittest.main()
