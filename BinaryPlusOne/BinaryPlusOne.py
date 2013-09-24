e1315af4dd004685a8d283ebc324e1877ca93513
Mark Berryman <markwberryman@yahoo.com>
2013-09-24 07:51:37 -0700

    simplifying algo greatly

--- BinaryPlusOne/BinaryPlusOne.py
+++ BinaryPlusOne/BinaryPlusOne.py
@@ -1,8 +1,7 @@
 class BinaryPlusOne(object):
     @staticmethod
     def add_one(num):
-        """Pass in an array representing an array of bits."""
-        # start from rightmost bit
+        """Pass in an array representing an array of bits."""        
         bitIndex = len(num) - 1
         
         while (True):
@@ -13,23 +12,3 @@ class BinaryPlusOne(object):
                 num[bitIndex] = 0
 
             bitIndex -= 1
-
-        ## look at rightmost bit
-        #rBit = num[bitIndex]
-        ## if it's a zero, replace w/ a one
-        #if (rBit == 0):
-        #    num[bitIndex] = 1
-        #else:
-        #    # if it's a one, replace w/ a zero
-        #    num[bitIndex] = 0
-        #    # now we're in the "carry" phase
-        #    while (True):
-        #        # look at next digit (going left)
-        #        bitIndex -= 1
-        #        # if it's a zero, replace w/ a one
-        #        if (num[bitIndex] == 0):
-        #            num[bitIndex] = 1
-        #            break
-        #        else:
-        #            # if it's a one, replace w/ a zero; continue
-        #            num[bitIndex] = 0
