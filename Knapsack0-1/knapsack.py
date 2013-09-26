2b4d1be14c6e285abd85a1b873caf3c8731d00bc
Mark Berryman <markwberryman@yahoo.com>
2013-09-26 15:37:20 -0700

    doc'ing the problem

--- Knapsack0-1/knapsack.py
+++ Knapsack0-1/knapsack.py
@@ -1,1 +1,10 @@
-print('Hello World')
+class knapsack(object):
+    """The Knapsack problem is a combinatorial optimization problem. Given
+    a set of items w/ weights and values optimize the selection of items
+    to maximize value w/o exceeding a specific weight.
+    Brute force algorithm is exponential - O(2^n). Knapsack algorithm using
+    dynamic programming technique is O(n * W) where n is the number of
+    items to consider and W is the maximum weight.
+    """
+    def __init__(self, maxWeight):
+        self.maxWeight = maxWeight
