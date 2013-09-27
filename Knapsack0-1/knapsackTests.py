cb9e06ad49ecec6f780c7ae029b7291bf7d7e087
Mark Berryman <markwberryman@yahoo.com>
2013-09-27 07:57:48 -0700

    +unit tests

--- Knapsack0-1/knapsackTests.py
+++ Knapsack0-1/knapsackTests.py
@@ -6,6 +6,13 @@ class CalculateValuesTests(unittest.TestCase):
     def test_KnapsackWithCapacityZero(self):
         sut = knapsack.Knapsack(0)
 
+        actual = sut.calculate_values([])
+
+        self.assertIsNone(actual)
+
+    def test_NoItems(self):
+        sut = knapsack.Knapsack(1)
+
         actual = sut.calculate_values(None)
 
         self.assertIsNone(actual)
