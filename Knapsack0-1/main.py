2f4bf4ee0e3d5b5d04f570eb45fade6437b43a10
Mark Berryman <markwberryman@yahoo.com>
2013-09-27 15:44:15 -0700

    adding realistic test data

--- Knapsack0-1/main.py
+++ Knapsack0-1/main.py
@@ -1,7 +1,11 @@
 import knapsack
 
-ks = knapsack.Knapsack(3)
-x = ks.calculate_values([(1,1),(2,2)])
+ks = knapsack.Knapsack(5)
+item1 = knapsack.Item(1, 2, 3)
+item2 = knapsack.Item(2, 3, 4)
+item3 = knapsack.Item(3, 4, 5)
+item4 = knapsack.Item(4, 5, 6)
+x = ks.calculate_values([item1, item2, item3, item4])
 ks.print_values(x)
 
 input('Press Enter to exit')
