30014b8480c13b5670058d2cda16adf39e9be8c9
Mark Berryman <markwberryman@yahoo.com>
2013-09-30 08:03:05 -0700

    +unit test

--- LongestCommonSubstr/longestCommonSubstrTests.py
+++ LongestCommonSubstr/longestCommonSubstrTests.py
@@ -10,7 +10,16 @@ class GenSubstringsTests(unittest.TestCase):
 
         actual = sut.genSubstrings(s)
 
-        self.assertEqual(0, len(actual.difference(expected)))
+        self.assertEqual(0, len(expected.difference(actual)))
+
+    def test_TwoCharString(self):
+        s = "xy"
+        expected = { "x", "y", "xy" }
+        sut = longestCommonSubstr.LongestCommonSubstr()
+
+        actual = sut.genSubstrings(s)
+
+        self.assertEqual(0, len(expected.difference(actual)))
 
 
 if __name__ == '__main__':
