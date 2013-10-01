3c5afb0fecfda88cd2d7b5b9dd3ca0d153d961d1
Mark Berryman <markwberryman@yahoo.com>
2013-10-01 08:19:35 -0700

    adding some timing tests to compare my RK implementation against built in string search

--- StringSearch/main.py
+++ StringSearch/main.py
@@ -1,1 +1,27 @@
-print('Hello World')
+import rabinKarp
+import time
+
+
+algo = rabinKarp.RabinKarp()
+s1 = "ABC"
+s2 = "1234567890ABC"
+numIters = 100000
+
+time.clock()
+
+for x in range(numIters):
+    algo.contains_string(s1, s2)
+
+timeRK = time.clock()
+
+#for x in range(numIters):
+#    s1 in s2
+
+#timeBuiltInStringSearch = time.clock() - timeRK
+
+#print("RK time: " + str(timeRK))
+#print("Built-in algo time: " + str(timeBuiltInStringSearch))
+#print()
+#print("Comparison (RK / Built-in): " + str((timeRK / timeBuiltInStringSearch)))
+
+#input("Done")
