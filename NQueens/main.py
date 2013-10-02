e7f62a1ad31059b005910a14210ab21bc6d0d1b2
Mark Berryman <markwberryman@yahoo.com>
2013-10-02 10:22:31 -0700

    +timing

--- NQueens/main.py
+++ NQueens/main.py
@@ -1,13 +1,19 @@
 import recursiveBacktracking
+import time
 
 rbt = recursiveBacktracking.RecursiveBacktracking()
 boardDimension = 8
 board = [[0 for x in range(boardDimension)] for x in range(boardDimension)]
+
+startTime = time.clock()
+
 result = rbt.solve(8, board)
 
 if (result is False):
     print("No solution")
 
 print("\nNumber of calculations = {:,}".format(rbt.numCalcs))
+
+print("\nTime taken (seconds): " + str(time.clock() - startTime))
 
 #input("Done")
