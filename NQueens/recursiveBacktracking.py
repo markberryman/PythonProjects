c2a5e4e740ef629ced9c92ca03c9ee481caa5ae8
Mark Berryman <markwberryman@yahoo.com>
2013-10-02 09:48:00 -0700

    [bf] - ugh, forgot to deep clone the board to prevent solutions from stomping on one another

--- NQueens/recursiveBacktracking.py
+++ NQueens/recursiveBacktracking.py
@@ -1,26 +1,34 @@
+import copy
+
+
 class RecursiveBacktracking(object):
     """Solving the N-Queens problem using a recursive backtracking algorithm."""
+    def __init__(self):
+        self.numCalcs = 0
+
     def print(self, board):
         for x in range(len(board)):
             print(board[x])
 
     def can_place_queen(self, board, row, col):
+        self.numCalcs += 1
+
         # check for exact space conflict
         if (board[row][col] == 1):
-            print("Exact space conflict.")
+            #print("Exact space conflict.")
             return False
 
         # check for row conflict
         rowToCheck = board[row]
         for i in rowToCheck:
             if (i == 1):
-                print("Row conflict.")
+                #print("Row conflict.")
                 return False
 
         # check for col conflict
         for i in range(len(board)):
             if (board[i][col] == 1):
-                print("Column conflict.")
+                #print("Column conflict.")
                 return False
 
         # check for diagonal conflict
@@ -30,7 +38,7 @@ class RecursiveBacktracking(object):
 
         while ((rowIdxToCheck >= 0) and (colIdxToCheck >= 0)):
             if (board[rowIdxToCheck][colIdxToCheck] == 1):
-                print("Diagonal conflict (UL).")
+                #print("Diagonal conflict (UL).")
                 return False
             else:
                 rowIdxToCheck -= 1
@@ -42,7 +50,7 @@ class RecursiveBacktracking(object):
 
         while ((rowIdxToCheck >= 0) and (colIdxToCheck < len(board[0]))):
             if (board[rowIdxToCheck][colIdxToCheck] == 1):
-                print("Diagonal conflict (UR).")
+                #print("Diagonal conflict (UR).")
                 return False
             else:
                 rowIdxToCheck -= 1
@@ -54,7 +62,7 @@ class RecursiveBacktracking(object):
 
         while ((rowIdxToCheck < len(board)) and (colIdxToCheck < len(board[0]))):
             if (board[rowIdxToCheck][colIdxToCheck] == 1):
-                print("Diagonal conflict (DR).")
+                #print("Diagonal conflict (DR).")
                 return False
             else:
                 rowIdxToCheck += 1
@@ -66,7 +74,7 @@ class RecursiveBacktracking(object):
 
         while ((rowIdxToCheck < len(board)) and (colIdxToCheck >= 0)):
             if (board[rowIdxToCheck][colIdxToCheck] == 1):
-                print("Diagonal conflict (DL).")
+                #print("Diagonal conflict (DL).")
                 return False
             else:
                 rowIdxToCheck += 1
@@ -77,24 +85,30 @@ class RecursiveBacktracking(object):
     def solve(self, numQueens, board):
         if (numQueens == 0):
             # solved
-            return True 
+            print("\nSolution:")
+            self.print(board)
+            return True
 
         # try to place a queen considering every spot on the board
         for row in range(len(board)):
             for col in range(len(board[0])):
                 # queen can be placed at row,col?
-                print("Considering [{},{}]".format(row, col))
+                #print("Considering [{},{}]".format(row, col))
                 canPlace = self.can_place_queen(board, row, col)
 
                 if (canPlace):
-                    print("Placing queen at [{},{}]".format(row, col))
-                    # update the board w/ the queen's placement
-                    board[row][col] = 1
+                    #print("Placing queen at [{},{}]. {} to go.".format(row, col, numQueens - 1))
 
-                    self.print(board)
+                    # need to copy the board so we don't have separate
+                    # solution investigations stomp on each other
+                    newBoard = copy.deepcopy(board)
+                    # update the board w/ the queen's placement
+                    newBoard[row][col] = 1
+
+                    #self.print(newBoard)
 
                     # recurse w/ one less queen
-                    result = self.solve(numQueens - 1, board)
+                    result = self.solve(numQueens - 1, newBoard)
 
                     if (result):
                         # we're done; no need trying to place queens
@@ -102,4 +116,5 @@ class RecursiveBacktracking(object):
                 # else: continue trying to place this queen
 
         # couldn't place this queen
+        #print("Backtracking...")
         return False
