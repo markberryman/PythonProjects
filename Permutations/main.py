2449ef3d7c06c0ac8a41d72b5e584cc949b6dbe6
Mark Berryman <markwberryman@yahoo.com>
2013-10-03 14:02:31 -0700

    first solution

--- Permutations/main.py
+++ Permutations/main.py
@@ -1,1 +1,28 @@
-print('Hello World')
+import math
+
+def inject_char(char, listOfStrings):
+    result = []
+
+    for s in listOfStrings:
+        for i in range(len(s) + 1):
+            result.append(s[:i] + char + s[i:])
+
+    return result
+
+def permute(s):
+    """ "abc" -> ["abc", "acb", "bac", "bca", "cab", "cba"] """
+    # base case
+    if (len(s) == 1):
+        return [s]
+
+    permutation = permute(s[1:])
+
+    return inject_char(s[0], permutation)
+
+
+
+s = "abcdefghi"
+permutations = permute(s)
+assert len(permutations) == math.factorial(len(s))
+print(permute(s))
+input("Done...")
