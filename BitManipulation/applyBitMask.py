0fe9dc36cbe605c8cbb43542f179d3f7d8f156c6
Mark Berryman <markwberryman@yahoo.com>
2013-10-04 10:19:56 -0700

    first implementation

--- BitManipulation/applyBitMask.py
+++ BitManipulation/applyBitMask.py
@@ -1,11 +1,40 @@
+import math
+
+
 class ApplyBitMask(object):
     """Given two 32-bit numbers N and M and a range from I to J,
        replace the bits in N in range I to J w/ the bits in M.
        For example:
        N = 10000000000
-       M = 00000010101
+       M = 10101
        I = 2, J= 6
        Result = 10000101010"""
 
+    def create_sub_mask(self, I, J):
+        """Creates a bit mask of all 1's except for
+        a range of 0's as specified in the range
+        between I and J. Note, they're 1-based."""
+        # start w/ all 1's
+        num = int(math.pow(2, 32) - 1)
+
+        # slap in the 0's
+        while (J >= I):
+            num -= int(math.pow(2, J - 1))
+            J -= 1
+
+        return num
+       
     def apply_mask(self, N, M, I, J):
-        """Note, J and I are 1 based."""
+        """Note, J and I are 1 based and J >= I."""
+        assert J >= I
+
+        # create mask that 0's out portion of N to replace w/ M
+        mask = self.create_sub_mask(I, J)
+
+        # apply mask
+        nPrime = N & mask
+
+        # scoot M over to the range area
+        mPrime = M << (I - 1)
+
+        return nPrime | mPrime
--- BitManipulation/applyBitMaskTests.py
+++ BitManipulation/applyBitMaskTests.py
@@ -1,10 +1,69 @@
 import applyBitMask
+import math
 import unittest
 
 
+class CreateSubMaskTests(unittest.TestCase):
+    def test_i1j1(self):
+        I = 1
+        J = 1
+        all1s = int(math.pow(2, 32) - 1)
+        expected = int(all1s - math.pow(2, 0))
+        sut = applyBitMask.ApplyBitMask()
+
+        actual = sut.create_sub_mask(I, J)
+
+        self.assertEqual(expected, actual)
+
+    def test_i1j2(self):
+        I = 1
+        J = 2
+        all1s = int(math.pow(2, 32) - 1)
+        expected = int(all1s - math.pow(2, 1) - math.pow(2, 0))
+        sut = applyBitMask.ApplyBitMask()
+
+        actual = sut.create_sub_mask(I, J)
+
+        self.assertEqual(expected, actual)
+
+    def test_i4j6(self):
+        I = 4
+        J = 6
+        all1s = int(math.pow(2, 32) - 1)
+        expected = int(all1s - math.pow(2, 5) - math.pow(2, 4) - math.pow(2, 3))
+        sut = applyBitMask.ApplyBitMask()
+
+        actual = sut.create_sub_mask(I, J)
+
+        self.assertEqual(expected, actual)
+
+    def test_i2j6(self):
+        I = 2
+        J = 6
+        all1s = int(math.pow(2, 32) - 1)
+        expected = int(all1s - math.pow(2, 5) - math.pow(2, 4) - math.pow(2, 3) - math.pow(2, 2) - math.pow(2, 1))
+        sut = applyBitMask.ApplyBitMask()
+
+        actual = sut.create_sub_mask(I, J)
+
+        self.assertEqual(expected, actual)
+
 class ApplyMaskTests(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+    def test_BaseCase(self):
+        # N = 10000000000
+        # M = 10101
+        # I = 2, J= 6
+        # R = 10000101010
+        N = int(math.pow(2, 10))
+        M = int(math.pow(2, 4) + math.pow(2, 2) + math.pow(2, 0))
+        I = 2
+        J = 6
+        expected = int(math.pow(2, 10) + math.pow(2, 5) + math.pow(2, 3) + math.pow(2, 1))
+        sut = applyBitMask.ApplyBitMask()
+
+        actual = sut.apply_mask(N, M, I, J)
+
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
