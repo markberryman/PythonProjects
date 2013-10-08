b17a4c65e6202c17af97f5cd7492c12b206c188f
Mark Berryman <markwberryman@yahoo.com>
2013-10-08 15:18:11 -0700

    +new proj

---
+++ MaxHeap/maxHeap.py
@@ -0,0 +1,50 @@
+# todo - implement max_heapify
+# todo - implement heapsort
+
+class maxHeap(object):
+    """Binary max-heap."""
+    def __init__(self, data):
+        """Data is an array of unordered integers."""
+        self.data = data
+
+    def get_child(self, i, dir):
+        """Returns array index of child of "i" element. "i" is 1 based.
+        Dir 0 means left. 1 means right."""
+        if (dir == 0):
+            return 2 * i
+        else:
+            return (2 * i) + 1
+
+    def max_heapify(self, i):
+        """Orders the data to satisfy the max-heap property:
+        Value of parent node >= value of child nodes.
+        Once we've done this, we can employ heap sort.
+        Parameter 'i' is the starting index ot begin the "heapfication"."""
+        lChildIdx = self.get_child(i, 0)
+        rChildIdx = self.get_child(i, 1)
+        largestIdx = i
+
+        # for element at 'i', see if either "child" is greater (i.e., voilates
+        # the max-heap property)
+
+        # index check is to prevent us from going out of bounds
+        # w/ the array and will cause largestIdx == i when we
+        # do which means we won't recurse
+        if ((lChildIdx <= len(self.data)) and 
+            (self.data[lChildIdx - 1] > self.data[i - 1])):
+            largestIdx = lChildIdx
+        else:
+            largestIdx = i
+
+        if ((rChildIdx <= len(self.data)) and 
+            (self.data[rChildIdx - 1] > self.data[largestIdx - 1])):
+            largestIdx = rChildIdx
+
+        # need to swap values?
+        if (largestIdx != i):
+            temp = self.data[i - 1]
+            self.data[i - 1] = self.data[largestIdx - 1]
+            self.data[largestIdx - 1] = temp
+
+            # we need to max_heapify the child element we swapped
+            self.max_heapify(largestIdx)
---
+++ MaxHeap/maxHeapTests.py
@@ -0,0 +1,52 @@
+import maxHeap
+import unittest
+
+
+class GetChildTests(unittest.TestCase):
+    def test_GetLeftChildOf3ElementHeap(self):
+        data = [3,2,1]
+        expected = 2
+        sut = maxHeap.maxHeap(data)
+
+        actual = sut.get_child(1, 0)
+
+        self.assertEqual(expected, actual)
+
+    def test_GetRightChildOf3ElementHeap(self):
+        data = [3,2,1]
+        expected = 3
+        sut = maxHeap.maxHeap(data)
+
+        actual = sut.get_child(1, 1)
+
+        self.assertEqual(expected, actual)
+
+
+class MaxHeapifyTests(unittest.TestCase):
+    def test_3ElementHeap(self):
+        data = [1,2,3]
+        expected = [3,2,1]
+        sut = maxHeap.maxHeap(data)
+
+        # remember, we're one based so we're pointing
+        # at the root here
+        sut.max_heapify(1)
+        actual = sut.data
+
+        self.assertEqual(expected, actual)
+
+    def test_10ElementHeapMaxHeapify2(self):
+        data = [16,4,10,14,7,9,3,2,8,1]
+        expected = [16,14,10,8,7,9,3,2,4,1]
+        sut = maxHeap.maxHeap(data)
+
+        # remember, we're one based so we're pointing
+        # at the root here
+        sut.max_heapify(2)
+        actual = sut.data
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
