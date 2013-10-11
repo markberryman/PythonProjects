56bfabd27f42455b963144f83d676b0acd4389f8
Mark Berryman <markwberryman@yahoo.com>
2013-10-11 08:52:23 -0700

    dropping node and edge classes; not needed; adding logic for adding edge to graph; adding and fixing unit tests

--- Graphs/adjListGraph.py
+++ Graphs/adjListGraph.py
@@ -1,23 +1,5 @@
-class Edge(object):
-    def __init__(self, id1, id2, w):
-        """Id's of the two nodes for the edge and a weight w."""
-        # todo - feeling too lazy to define properties...
-        self.id1 = id1
-        self.id2 = id2
-        self.w = w
-
-
-class Node(object):
-    def __init__(self, id):
-        self.__id = id
-
-    @property
-    def id(self):
-        return self.__id
-
-
 class AdjListGraph(object):
-    """Graph structure stored in the adjacency list form.
+    """Undirected graph structure stored in the adjacency list form.
     Using a dictionary for the data structure so we have
     fast lookup of nodes. The value of each element
     in the dictionary will be a list of the node's edges."""
@@ -28,13 +10,19 @@ class AdjListGraph(object):
     def graph(self):
         return self.__graph
 
-    def add_node(self, node):
-        if (node.id not in self.__graph):
-            self.__graph[node.id] = []
+    def add_node(self, id):
+        """Add a vertice with the given id."""
+        if (id not in self.__graph):
+            self.__graph[id] = []
         else:
             raise ValueError("Can't add duplicate node.")
 
-    def add_edge(self, edge):
-        if ((edge.id1 not in self.__graph) or
-            (edge.id2 not in self.__graph)):
+    def add_edge(self, id1, id2, w):
+        """Add an edge between nodes w/ given id's and weight "w"."""
+        if ((id1 not in self.__graph) or
+            (id2 not in self.__graph)):
             raise ValueError("Can't add edge for non-existant node.")
+
+        # undirected graph so add two edges
+        self.__graph[id1].append([id2, w])
+        self.__graph[id2].append([id1, w])
--- Graphs/adjListGraphTests.py
+++ Graphs/adjListGraphTests.py
@@ -4,49 +4,54 @@ import unittest
 
 class AddEdgeTests(unittest.TestCase):
     def test_AddEdgeForNonExistantFirstNode(self):
-        edge1 = adjListGraph.Edge(0, 1, 0)
         sut = adjListGraph.AdjListGraph()
 
-        self.assertRaises(ValueError, sut.add_edge, edge1)
+        self.assertRaises(ValueError, sut.add_edge, 0, 1, 50)
 
     # not a true unit test b/c we're adding a node
     # todo - mock this call
-    def test_AddEdgeForNonExistantFirstNode(self):
-        edge1 = adjListGraph.Edge(0, 1, 0)
+    def test_AddEdgeForNonExistantSecondNode(self):
         sut = adjListGraph.AdjListGraph()
-        node1 = adjListGraph.Node(0)
-        sut.add_node(node1)
+        sut.add_node(0)
 
-        self.assertRaises(ValueError, sut.add_edge, edge1)
+        self.assertRaises(ValueError, sut.add_edge, 0, 1, 50)
+
+    # not a true unit test b/c we're adding a node
+    # todo - mock this call
+    def test_AddEdgeBetweenNodes(self):
+        sut = adjListGraph.AdjListGraph()
+        sut.add_node(0)
+        sut.add_node(1)
+
+        sut.add_edge(0, 1, 50)
+
+        self.assertEqual(sut.graph[0][0], [1, 50])
+        self.assertEqual(sut.graph[1][0], [0, 50])
 
 
 class AddNodeTests(unittest.TestCase):
     def test_AddNewNodeToGraphAddsNode(self):
-        node = adjListGraph.Node(0)
         sut = adjListGraph.AdjListGraph()
 
-        sut.add_node(node)
+        sut.add_node(0)
 
         self.assertEqual(1, len(sut.graph))
 
     def test_AddDupeNodeRaisesExc(self):
-        node1 = adjListGraph.Node(0)
-        node2 = adjListGraph.Node(0)
         sut = adjListGraph.AdjListGraph()
 
-        sut.add_node(node1)
+        sut.add_node(0)
 
-        self.assertRaises(ValueError, sut.add_node, node2)
+        self.assertRaises(ValueError, sut.add_node, 0)
 
     def test_AddingTwoNodesToGraphAddsTwoNodes(self):
-        node1 = adjListGraph.Node(0)
-        node2 = adjListGraph.Node(1)
         sut = adjListGraph.AdjListGraph()
 
-        sut.add_node(node1)
-        sut.add_node(node2)
+        sut.add_node(0)
+        sut.add_node(1)
 
         self.assertEqual(2, len(sut.graph))
+
 
 if __name__ == '__main__':
     unittest.main()
