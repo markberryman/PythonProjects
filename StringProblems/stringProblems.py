a1ecc96d08152c383e7d3e95187ad7599bc90552
Mark Berryman <markwberryman@yahoo.com>
2013-10-12 15:51:25 -0700

    adding remove dupes method; +tests

--- StringProblems/stringProblems.py
+++ StringProblems/stringProblems.py
@@ -1,5 +1,49 @@
 class StringProblems(object):
     @staticmethod
+    def remove_dupes(s):
+        """No additional space beyond a few small tracking vars.
+        Minimal number of operations to "compact" the array."""
+        # can't have dupes if string only one char; special case
+        if ((s is None) or (len(s) < 2)):
+            return s
+
+        tail = 0    # tracks last position of a unique character
+
+        # we'll look at each character except the first; no need
+        for i in range(1, len(s)):
+            for j in range(i + 1):
+                # we'll look at all characters before that character
+                if (s[i] == s[j]):
+                    # dupe char
+                    break
+
+            if (j == i):
+                # unique char
+                tail += 1
+                s[tail] = s[i]
+
+        # we're done checking characters
+        # drop everything after the tail
+        # since we can't drop a null terminator to
+        # designate the end of the string, we'll have
+        # to create a new string
+        return s[:tail + 1]
+
+    @staticmethod
+    def reverse_string(s):
+        """Reverse string in place."""
+        startIdx = 0
+        endIdx = len(s) - 1
+
+        while (endIdx > startIdx):
+            temp = s[startIdx]
+            s[startIdx] = s[endIdx]
+            s[endIdx] = temp
+
+            startIdx += 1
+            endIdx -= 1
+
+    @staticmethod
     def is_all_unique_chars(s):
         """This works if we know all chars are in the extended ASCII
         character set. Uses a small amount of add'l space but O(n)."""
--- StringProblems/stringProblemsTests.py
+++ StringProblems/stringProblemsTests.py
@@ -2,6 +2,82 @@ import stringProblems
 import unittest
 
 
+class RemoveDupes(unittest.TestCase):
+    def test_NoneString(self):
+        s = None
+        expected = None
+
+        actual = stringProblems.StringProblems.remove_dupes(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_OneCharString(self):
+        s = ['a']
+        expected = ['a']
+
+        actual = stringProblems.StringProblems.remove_dupes(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_TwoCharStringNoDupes(self):
+        s = ['a', 'b']
+        expected = ['a', 'b']
+
+        actual = stringProblems.StringProblems.remove_dupes(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_TwoCharStringWithDupes(self):
+        s = ['a', 'a']
+        expected = ['a']
+
+        actual = stringProblems.StringProblems.remove_dupes(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_ThreeCharStringAllDupes(self):
+        s = ['a', 'a', 'a']
+        expected = ['a']
+
+        actual = stringProblems.StringProblems.remove_dupes(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_FourCharStringStringWithPairOfDupes(self):
+        s = ['a', 'a', 'b', 'b']
+        expected = ['a', 'b']
+
+        actual = stringProblems.StringProblems.remove_dupes(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_LongStringWithSpreadOutDupes(self):
+        s = ['a', 'b', 'c', 'b', 'a', 'c', 'd', 'a']
+        expected = ['a', 'b', 'c', 'd']
+
+        actual = stringProblems.StringProblems.remove_dupes(s)
+
+        self.assertEqual(expected, actual)
+
+
+class ReverseString(unittest.TestCase):
+    def test_ReversesOneCharString(self):
+        s = ['a']
+        expected = ['a']
+        
+        stringProblems.StringProblems.reverse_string(s)
+
+        self.assertEqual(expected, s)
+
+    def test_ReversesTwoCharString(self):
+        s = ['a', 'b']
+        expected = ['b', 'a']
+        
+        stringProblems.StringProblems.reverse_string(s)
+
+        self.assertEqual(expected, s)
+
+
 class IsAllUniqueCharsTests(unittest.TestCase):
     def test_ReturnsTrueWhenStringIsAllUniqueChars(self):
         expected = True
