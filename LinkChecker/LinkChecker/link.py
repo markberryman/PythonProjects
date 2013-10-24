d44af2e6a097b83823aeb273250432cc8498bfde
Mark Berryman <markwberryman@yahoo.com>
2013-10-24 14:14:13 -0700

    removing LinkChecker proj now that it's in a separate repo

--- LinkChecker/LinkChecker/link.py
+++
@@ -1,9 +0,0 @@
-import linkType
-
-
-class Link(object):
-    """Represents a link to a resource"""
-
-    def __init__(self, url, type=linkType.LinkType.ANCHOR):
-        self.url = url
-        self.type = type
--- LinkChecker/LinkChecker/linkType.py
+++
@@ -1,5 +0,0 @@
-class LinkType(object):
-    ANCHOR = 1
-    STYLESHEET = 2
-    SCRIPT = 3
-    IMAGE = 4
