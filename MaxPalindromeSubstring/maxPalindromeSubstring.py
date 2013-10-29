475d1496ba115ff3b18a39801a9980ad77f98be0
Mark Berryman <markwberryman@yahoo.com>
2013-10-29 09:24:05 -0700

    adding logic for handling odd length palindrome strings

--- MaxPalindromeSubstring/maxPalindromeSubstring.py
+++ MaxPalindromeSubstring/maxPalindromeSubstring.py
@@ -23,9 +23,33 @@ def find_max_palindrome_substring(s):
                     j += 1
 
             if (j > 0):
-                # even length palindrome case
+                # even length palindrome
                 currentPalindrome = s[(i - j + 1):(i + 1 + j)]
-    
+
+            if (len(currentPalindrome) > len(largestPalindrome)):
+                largestPalindrome = currentPalindrome
+
+            j = 0
+
+            # check for the odd case by looking at char to the
+            # left and right of 'i'
+            if (((i + 1) < len(s)) and
+                ((i - 1) >= 0) and
+                (s[i - 1] == s[i + 1])):
+                j += 2
+
+                while (((i - j) >= 0) and
+                       ((i + j) < len(s))):
+                    if (s[i -  j] != s[i + j]):
+                        break;
+
+                    j += 1
+
+            # calculate palindrome
+            if (j > 0):
+                # odd length palindrome
+                currentPalindrome = s[(i - j + 1):(i + j)]
+
             if (len(currentPalindrome) > len(largestPalindrome)):
                 largestPalindrome = currentPalindrome
     else:
--- MaxPalindromeSubstring/test_MaxPalindromeSubstring.py
+++ MaxPalindromeSubstring/test_MaxPalindromeSubstring.py
@@ -2,6 +2,7 @@ import maxPalindromeSubstring
 import unittest
 
 class MaxPalindromeSubstringTest(unittest.TestCase):
+    # even length palindrome tests
     def test_SingleCharString(self):
         s = "x"
         expected = "x"
@@ -45,6 +46,48 @@ class MaxPalindromeSubstringTest(unittest.TestCase):
     def test_SixCharStringThatHasFourCharPalindromeInMiddle(self):
         s = "AxxxxB"
         expected = "xxxx"
+
+        actual = maxPalindromeSubstring.find_max_palindrome_substring(s)
+
+        self.assertEqual(expected, actual)
+
+    # odd length palindrome tests
+    def test_ThreeCharStringThatHasThreeCharPalindrome(self):
+        s = "xxx"
+        expected = "xxx"
+
+        actual = maxPalindromeSubstring.find_max_palindrome_substring(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_FourCharStringThatHasThreeCharPalindromeAtEnd(self):
+        s = "Axxx"
+        expected = "xxx"
+
+        actual = maxPalindromeSubstring.find_max_palindrome_substring(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_FourCharStringThatHasThreeCharPalindromeAtFront(self):
+        s = "xxxA"
+        expected = "xxx"
+
+        actual = maxPalindromeSubstring.find_max_palindrome_substring(s)
+
+        self.assertEqual(expected, actual)
+
+    def test_FiveCharStringThatHasThreeCharPalindromeInMiddle(self):
+        s = "AxxxB"
+        expected = "xxx"
+
+        actual = maxPalindromeSubstring.find_max_palindrome_substring(s)
+
+        self.assertEqual(expected, actual)
+
+    # strings w/ a mix of even and odd length palindromes
+    def test_StringWithOddLengthPalindromeAsLargest(self):
+        s = "ABBAracecarABA"
+        expected = "racecar"
 
         actual = maxPalindromeSubstring.find_max_palindrome_substring(s)
 
