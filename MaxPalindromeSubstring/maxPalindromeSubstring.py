13b9256398e86c2bc075d33211a360d3750c4606
Mark Berryman <markwberryman@yahoo.com>
2013-10-29 16:45:40 -0700

    refactoring palindrome search to single method

--- MaxPalindromeSubstring/maxPalindromeSubstring.py
+++ MaxPalindromeSubstring/maxPalindromeSubstring.py
@@ -16,58 +16,24 @@ def find_palindrome_from_center(s, left_idx, right_idx):
 
 def find_max_palindrome_substring(s):
     largestPalindrome = ""
-    currentPalindrome = ""
     
     if (len(s) != 1):
         for i in range(len(s)):
             # look at each character in the string
             # for each character, evaluate if we're looking
             # at a possible even length or odd length palindrome
-            j = 0
 
-            # check for even case by looking char to right of 'i'
-            # for a match
-            if (((i + 1) < len(s)) and
-                (s[i] == s[i + 1])):
-                j += 1
+            # even case
+            evenPalindrome = find_palindrome_from_center(s, i, i + 1)
+            oddPalindrome = find_palindrome_from_center(s, i, i)
 
-                while (((i - j) >= 0) and
-                       ((i + 1) + j) < len(s)):
-                    if (s[i -  j] != s[(i + 1) + j]):
-                        break;
+            if ((len(evenPalindrome) > len(oddPalindrome)) and
+                (len(evenPalindrome) > len(largestPalindrome))):
+                largestPalindrome = evenPalindrome
 
-                    j += 1
-
-            if (j > 0):
-                # even length palindrome
-                currentPalindrome = s[(i - j + 1):(i + 1 + j)]
-
-            if (len(currentPalindrome) > len(largestPalindrome)):
-                largestPalindrome = currentPalindrome
-
-            j = 0
-
-            # check for the odd case by looking at char to the
-            # left and right of 'i'
-            if (((i + 1) < len(s)) and
-                ((i - 1) >= 0) and
-                (s[i - 1] == s[i + 1])):
-                j += 2
-
-                while (((i - j) >= 0) and
-                       ((i + j) < len(s))):
-                    if (s[i -  j] != s[i + j]):
-                        break;
-
-                    j += 1
-
-            # calculate palindrome
-            if (j > 0):
-                # odd length palindrome
-                currentPalindrome = s[(i - j + 1):(i + j)]
-
-            if (len(currentPalindrome) > len(largestPalindrome)):
-                largestPalindrome = currentPalindrome
+            if ((len(oddPalindrome) > len(evenPalindrome)) and
+                (len(oddPalindrome) > len(largestPalindrome))):
+                largestPalindrome = oddPalindrome
     else:
         largestPalindrome = s
 
