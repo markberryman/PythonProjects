3e28d85f8e50e4efe4b0e5402dae69ace3645449
Mark Berryman <markwberryman@yahoo.com>
2013-11-25 15:44:47 -0800

    doing explicit returns of values to simplify if/else nesting

--- OneMonthDiffBetweenDates/oneMonthDiffBetweenDates.py
+++ OneMonthDiffBetweenDates/oneMonthDiffBetweenDates.py
@@ -31,53 +31,49 @@ def month_diff_bw_dates(date1, date2):
     """If two dates are more than a month apart, returns 1.
     Exactly a month apart (based on day value for each date), returns 0.
     Less than a month apart, returns -1."""
-    result = 0
-
     # more than a month
     # years differ too much
     if (math.fabs(date1.year - date2.year) > 1):
-        result = 1
+        return 1
+    
+    # detect year boundary scenario
+    if (math.fabs(date1.year - date2.year) == 1):
+        # requires knowing which date is smaller
+        smaller_date, larger_date = determine_smaller_date(date1, date2)
+        
+        # years differ, but we've got the boundary scenario of
+        # 12/x and 1/(x+1); 
+        if ((smaller_date.month == 12) and (larger_date.month == 1)):
+            # more than a month if days are too far apart
+            if (larger_date.day > smaller_date.day):
+                return 1
+
+            # exactly a month
+            if (larger_date.day == smaller_date.day):
+                return 0
+            
+            # less than a month
+            return -1
     else:
-        # detect year boundary scenario
-        if (math.fabs(date1.year - date2.year) == 1):
+        # years are the same
+        # difference b/w month values is > 1
+        if (math.fabs(date1.month - date2.month) > 1):
+            return 1
+        
+        # years are the same, month 1 apart
+        if (math.fabs(date1.month - date2.month) == 1):
             # requires knowing which date is smaller
             smaller_date, larger_date = determine_smaller_date(date1, date2)
+
+            # more than a month if days are too far apart
+            if (larger_date.day > smaller_date.day):
+                return 1
+            
+            # exactly a month
+            if (larger_date.day == smaller_date.day):
+                return 0
+
+            # less than a month
+            return -1
         
-            # years differ, but we've got the boundary scenario of
-            # 12/x and 1/(x+1); 
-            if ((smaller_date.month == 12) and (larger_date.month == 1)):
-                # more than a month if days are too far apart
-                if (larger_date.day > smaller_date.day):
-                    result = 1
-                else:
-                    # exactly a month
-                    if (larger_date.day == smaller_date.day):
-                        result = 0
-                    else:
-                        # less than a month
-                        result = -1
-        else:
-            # years are the same
-            # difference b/w month values is > 1
-            if (math.fabs(date1.month - date2.month) > 1):
-                result = 1
-            else:
-                # years are the same, month 1 apart
-                if (math.fabs(date1.month - date2.month) == 1):
-                    # requires knowing which date is smaller
-                    smaller_date, larger_date = determine_smaller_date(date1, date2)
-
-                    # more than a month if days are too far apart
-                    if (larger_date.day > smaller_date.day):
-                        result = 1
-                    else:
-                        # exactly a month
-                        if (larger_date.day == smaller_date.day):
-                            result = 0
-                        else:
-                            # less than a month
-                            result = -1
-                else:
-                    result = -1
-
-    return result
+        return -1
